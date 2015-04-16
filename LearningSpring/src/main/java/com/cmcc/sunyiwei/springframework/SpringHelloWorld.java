1f66ee64507fa66b9f68665e1584fb5e05d86e7b
SunYiwei <514912821@qq.com>
2015-04-17 00:10:00 +0800

    1. applicationEvent 2. customEvent 3.java-based configuration 4.Annotation-based configuration

--- LearningSpring/src/main/java/com/cmcc/sunyiwei/springframework/SpringHelloWorld.java
+++ LearningSpring/src/main/java/com/cmcc/sunyiwei/springframework/SpringHelloWorld.java
@@ -8,8 +8,10 @@
 package com.cmcc.sunyiwei.springframework;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
 
 /**
  * @ClassName: SpringHelloWorld
@@ -23,9 +25,13 @@ public class SpringHelloWorld {
 		// testScope();
 		// testInitAndDestructMethods();
 		// testAnotherInitAndDestructMethods();
-//		testBeanPostInitialization();
-//		testInheritance();
-		testInnerBeanInjection();
+		// testBeanPostInitialization();
+		// testInheritance();
+		// testInnerBeanInjection();
+		// testInjectCollection();
+		// testAutowire();
+//		testJavaBasedConfiguration();
+		testCustomEvent();
 	}
 
 	public static void testScope() {
@@ -75,21 +81,22 @@ public class SpringHelloWorld {
 		System.out.println(helloWorld_.getName());
 		applicationContext.registerShutdownHook();
 	}
-	
-	public static void testInheritance(){
+
+	public static void testInheritance() {
 		AbstractApplicationContext applicationContext = new ClassPathXmlApplicationContext(
 				"inheritance.xml");
 		HelloEnglish helloEnglish = (HelloEnglish) applicationContext
 				.getBean("helloEnglish");
-		HelloIndia helloIndia = (HelloIndia)applicationContext.getBean("helloIndia");
-		
+		HelloIndia helloIndia = (HelloIndia) applicationContext
+				.getBean("helloIndia");
+
 		System.out.println("helloEnglish says:" + helloEnglish.getName());
 		System.out.println("helloIndia says:" + helloIndia.getName());
 		System.out.println("helloIndia says:" + helloIndia.getMessage());
 		applicationContext.registerShutdownHook();
 	}
-	
-	public static void testInnerBeanInjection(){
+
+	public static void testInnerBeanInjection() {
 		AbstractApplicationContext applicationContext = new ClassPathXmlApplicationContext(
 				"innerBeanInjection.xml");
 		TextEditor textEditor = (TextEditor) applicationContext
@@ -97,4 +104,57 @@ public class SpringHelloWorld {
 
 		textEditor.spellCheck();
 	}
+
+	public static void testInjectCollection() {
+		AbstractApplicationContext applicationContext = new ClassPathXmlApplicationContext(
+				"collection.xml");
+		TestJavaCollection javaCollection = (TestJavaCollection) applicationContext
+				.getBean("javaCollection");
+
+		javaCollection.getAddressList();
+		javaCollection.getAddressMap();
+		javaCollection.getAddressProperties();
+		javaCollection.getAddressSet();
+		System.out.println("Name: " + javaCollection.getName());
+
+		applicationContext.registerShutdownHook();
+	}
+
+	public static void testAutowire() {
+		AbstractApplicationContext applicationContext = new ClassPathXmlApplicationContext(
+				"autoWire.xml");
+		TextEditor textEditor = (TextEditor) applicationContext
+				.getBean("textEditor");
+
+		textEditor.spellCheck();
+		System.out.println("Name: " + textEditor.getName());
+	}
+
+	public static void testJavaBasedConfiguration() {
+		/*
+		 * ApplicationContext applicationContext = new
+		 * AnnotationConfigApplicationContext( BeansConfig.class);
+		 */
+		AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+		applicationContext.register(BeansConfig.class);
+		applicationContext.refresh();
+		applicationContext.start();
+
+		TextEditor textEditor = (TextEditor) applicationContext
+				.getBean("textEditor");
+		textEditor.spellCheck();
+		applicationContext.stop();
+	}
+	
+	public static void testCustomEvent(){
+		AnnotationConfigApplicationContext ctx =
+				new AnnotationConfigApplicationContext();
+		ctx.register(BeansConfig.class);
+		ctx.refresh();
+		
+		CustomEventPublisher cep  =
+				(CustomEventPublisher)ctx.getBean("customEventPublisher");
+		cep.publish();
+		ctx.stop();
+	}	
 }
--- LearningSpring/src/main/java/com/cmcc/sunyiwei/springframework/TextEditor.java
+++ LearningSpring/src/main/java/com/cmcc/sunyiwei/springframework/TextEditor.java
@@ -1,19 +1,39 @@
 package com.cmcc.sunyiwei.springframework;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Required;
+
 public class TextEditor {
 	private SpellCheckor spellCheckor;
 
-	public void spellCheck(){
+	@Autowired(required = false)
+	@Qualifier("sunyiwei")
+	private String name;
+
+	public TextEditor(SpellCheckor spellCheckor) {
+		this.spellCheckor = spellCheckor;
+	}
+
+	public void spellCheck() {
 		spellCheckor.spellCheck();
 	}
-	
+
 	public SpellCheckor getSpellCheckor() {
 		return spellCheckor;
 	}
 
+	@Autowired
 	public void setSpellCheckor(SpellCheckor spellCheckor) {
 		this.spellCheckor = spellCheckor;
 	}
-	
-	
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
 }
